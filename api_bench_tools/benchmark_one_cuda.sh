8fa409067aa9ce9693d0617f934b1ca014e5c80f
Zhiyu Zhao <zhaozhiyu@sensetime.com>
2024-10-15 21:17:59 +0800

    [feature] add non-repetitive dataset sampling

--- api_bench_tools/benchmark_one_cuda.sh
+++ api_bench_tools/benchmark_one_cuda.sh
@@ -3,10 +3,16 @@
 SCRIPT=$(realpath -s "$0")
 PERF_BASE_PATH=$(dirname "$SCRIPT")
 
-if [ $ENABLE_SYSTEM_PROMPT -eq 0 ]; then
-    export BENCHMARK_EXTENDED_OPTIONS=
+if [ $ENABLE_SYSTEM_PROMPT -eq 1 ]; then
+    export BENCHMARK_EXTENDED_OPTIONS="$BENCHMARK_EXTENDED_OPTIONS --system-prompt-path $SYSTEM_PROMPT_PATH"
 else
-    export BENCHMARK_EXTENDED_OPTIONS="--system-prompt-path $SYSTEM_PROMPT_PATH"
+    export BENCHMARK_EXTENDED_OPTIONS=$BENCHMARK_EXTENDED_OPTIONS
+fi
+
+if [ $ALLOW_REPETITIVE_REQUESTS -eq 1 ]; then
+    export BENCHMARK_EXTENDED_OPTIONS="$BENCHMARK_EXTENDED_OPTIONS --allow-repetitive-requests"
+else
+    export BENCHMARK_EXTENDED_OPTIONS=$BENCHMARK_EXTENDED_OPTIONS
 fi
 
 bash "$PERF_BASE_PATH/benchmark_client_templ_cuda.sh" $*
